ce7023d85c56fa6fbccb95c48d5f2f4b517be6c6
David McElligott <damc.dev@gmail.com>
2015-02-09 18:56:23 -0600

    Added mocha test

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+exports.start = function() {
 var restify = require('restify');
 
 var server = restify.createServer({
@@ -9,10 +10,11 @@ server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 server.get('/user/:userId', function (req, res, next) {
-  res.send(req.params);
+  res.send({code: 200, message: req.params});
   return next();
 });
 
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
+};
---
+++ test/user_service_tests.js
@@ -0,0 +1,26 @@
+var client = restify.createJsonClient({
+    version: '*',
+    url: 'http://127.0.0.1:8080'
+});
+
+
+describe('service: user', function() {
+    
+    describe('200 response check', function() {
+        it('should get a 200 response', function(done) {
+            client.get('/user/1', function(err, req, res, data) {
+                if(err) {
+                    throw new Error(err);
+                } else {
+                    if(data.code != 200) {
+                        throw new Error('invalid response from /user/1');
+                    }
+                    done();
+                }
+            });
+
+        });
+
+    });
+
+});
